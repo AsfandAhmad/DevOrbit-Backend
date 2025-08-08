f428382b8b03220baafd2691316f424cae54d3b4
AsfandAhmad <mrasfandahmed@gmail.com>
2025-08-09 00:56:46 +0500

    Prepare for Railway deployment

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
@@ -5,13 +6,12 @@ const path = require('path');
 const app = express();
 
 // Connect to the database
-connectDB();
+connectDB(process.env.MONGO_URI);
 
-//Init MiddleWare
+// Middleware
 app.use(express.json({ extended: false }));
 
-
-// Define routes
+// Routes
 app.use('/api/users', require('./routes/API/users'));
 app.use('/api/auth', require('./routes/API/auth'));
 app.use('/api/profile', require('./routes/API/profile'));
@@ -19,7 +19,6 @@ app.use('/api/post', require('./routes/API/post'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-    // Set static folder
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
@@ -27,5 +26,4 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
